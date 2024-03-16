e0ebf38a8147a8b551687aff07a98e7769ced849
Eufra99 <eufrazinocunhaDev99@gmail.com>
2024-03-16 05:14:08 +0000

    add all functions

---
+++ app/src/main/java/com/example/todolist/NewTaskSheet.kt
@@ -0,0 +1,51 @@
+package com.example.todolist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.todolist.databinding.FragmentNewTaskSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+
+class NewTaskSheet : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentNewTaskSheetBinding
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val activity = requireActivity()
+        taskViewModel = ViewModelProvider(activity).get(TaskViewModel::class.java)
+        binding.saveButton.setOnClickListener {
+            saveAction()
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentNewTaskSheetBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    private fun saveAction() {
+        taskViewModel.name.value = binding.name.text.toString()
+        taskViewModel.desc.value = binding.desc.text.toString()
+        binding.name.setText("")
+        binding.desc.setText("")
+        dismiss()
+    }
+
+
+}
